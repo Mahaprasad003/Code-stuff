436a2ccfdb6d8e1e90fcabf5efb199fa76f4bd68
Mahaprasad Mohanty <mahaprasad003@gmail.com>
2021-11-19 11:58:30 +0530

    some random arrangements

---
+++ Experiments/Exp 2/compare_two.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the first number(A): ";
+    cin>>a;
+
+    cout<<"Enter the second number(B): ";
+    cin>>b;
+
+    if (a>b)
+    {
+        cout<<"A is greater than B";
+    }
+    else
+    cout<<"B is greater than A";  
+    
+    return 0;  
+}
