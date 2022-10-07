80c8b48378aa5215ee30b91bdc4ba25b6f489429
Mahaprasad Mohanty <mahaprasad003@gmail.com>
2022-10-07 20:07:49 +0530

    Learnt OOP using CPP

--- OOP_C++/file.cpp
+++ OOP_C++/file.cpp
@@ -1,16 +1,130 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Employee
+class AbstractEmployee
 {
-	string Name;
-	string Company;
-	int Age;
+     virtual void AskforPromotion() = 0;
+};
+
+class Employee:AbstractEmployee
+{
+private:
+     string Company;
+     int Age;
+protected:
+     string Name;
+
+public:
+     void setName(string name){
+          Name = name;
+     }
+     string getName(){
+          return Name;
+     }
+     void setCompany(string company){
+          Company = company;
+     }
+     string getCompany(){
+          return Company;
+     }
+     void setAge(int age){
+          if(age>=18)
+               Age = age;
+     }
+     int getAge(){
+          return Age;
+     }
+     void introduction()
+     {
+          cout << "Name - " << Name << endl;
+          cout << "Company - " << Company << endl;
+          cout << "Age - " << Age << endl;
+     }
+
+     // Creating a constructor
+     Employee(string s, string c, int a)
+     {
+          Name = s;
+          Company = c;
+          Age = a;          
+     }
+
+     void AskforPromotion(){
+          if(Age>30)
+               cout<<Name<<" got promoted!"<<endl;
+          else
+               cout<<Name<<" no promotion for you!"<<endl;
+
+     }
+
+     virtual void Work()
+     {
+          cout<<Name<<" is checking email, task backlog."<<endl;
+     }
+};
+
+class Developer : public Employee
+{
+public:
+     string FavProgrammingLang;
+
+     Developer(string s, string c, int a, string favProgrammingLang)
+          :Employee(s, c, a)
+     {
+               FavProgrammingLang = favProgrammingLang;          
+     }
+
+     void FixBug()
+     {
+          cout<<Name<<" fixed bug using "<<FavProgrammingLang<<endl;
+     }
+
+     void Work()
+     {
+          cout<<Name<<" is writing "<<FavProgrammingLang<<" code."<<endl;
+     }
+};
+
+class Teacher : public Employee
+{
+public:
+     string Subject;
+     void PrepareLesson()
+     {
+          cout<<Name<<" is preparing "<<Subject<<" lesson"<<endl;
+     }
+     Teacher(string s, string c, int a, string subject)
+          : Employee(s,c,a)
+     {
+          Subject = subject;
+     }
+
+     void Work()
+     {
+          cout<<Name<<" is teaching "<<Subject<<endl;
+     }
 };
 
 int main()
 {
-	Employee emp1;
-	emp1.
-	
+     // Employee emp1 = Employee("Mahaprasad", "AlphaGrep", 20);    
+     // Employee emp2 = Employee("John", "Amazon", 32);
+     Developer dev1 = Developer("Adam", "Google", 45, "Java");
+     // dev1.FixBug();
+     // dev1.AskforPromotion();
+     
+     Teacher teach1 = Teacher("Smita", "AECS", 40, "Chemistry.");
+     // teach1.PrepareLesson();
+     // teach1.AskforPromotion();
+
+     Employee *e1 = &dev1;
+     Employee *e2 = &teach1;
+
+     e1 -> Work();     ;
+     e2 -> Work();
+     
+     // emp1.setAge(15);
+     // cout<<emp1.getName()<<" is "<<emp1.getAge()<<" years old."<<endl;
+     
+
 }
---
+++ cpp/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+emp1.Name = "Mahaprasad";
+     // emp1.Company = "AlphaGrep";
+     // emp1.Age = 20;
