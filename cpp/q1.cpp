687c374ea1ae912cde9694831cdb10f7736e4d34
Mahaprasad Mohanty <mahaprasad003@gmail.com>
2022-10-07 13:40:25 +0530

    testing

--- cpp/q1.cpp
+++ cpp/q1.cpp
@@ -1,8 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-     for(int i=0;i<40000;i++)
-          cout<<i<<endl;
+     int n;
+     cin>>n;
+     
 }
