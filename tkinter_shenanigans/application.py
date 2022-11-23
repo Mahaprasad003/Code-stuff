ce7d01ed15c6e4b2f4264f9a5b5b6772ad29732a
Mahaprasad Mohanty <mahaprasad003@gmail.com>
2022-11-23 16:51:48 +0530

    idk man

--- tkinter_shenanigans/application.py
+++ tkinter_shenanigans/application.py
@@ -1,3 +1,16 @@
 import customtkinter as ct
+import tkinter as tk
+from tkcalendar import Calendar, DateEntry
 
-ct.set
+ct.set_appearance_mode("System")
+ct.set_default_color_theme("green")
+
+app = ct.CTk()
+app.geometry("800x600")
+
+label = ct.CTkLabel(app, text="Hey there!", text_font="Arial")
+label.pack()
+
+cal = DateEntry(app, width = 16, background = "magenta3", foreground = "white")
+cal.pack(padx = 20, pady = 20)
+app.mainloop()
